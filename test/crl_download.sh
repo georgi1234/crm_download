722b1ac68d47fe2e833a85ff7ef23af87d64a131
Georgi <30492692+georgi1234@users.noreply.github.com>
2021-02-22 16:28:35 +0200

    Create crl_download.sh

---
+++ test/crl_download.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##Created by Georgi Dimitrov
+##CRL download and check
+
+#vars
+export home=/app/ips-node/etc/crl-dir/
+download1=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalQCA.crl -O $home/new/B-TrustTestOperationalQCA.crl)
+download2=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalACA.crl -O $home/new/B-TrustTestOperationalACA.crl)
+new_crl1=/app/ips-node/etc/crl-dir/new/B-TrustTestOperationalQCA.crl
+new_crl2=/app/ips-node/etc/crl-dir/new/B-TrustTestOperationalACA.crl
+old_crl1=/app/ips-node/etc/crl-dir/B-TrustTestOperationalQCA.crl
+old_crl2=/app/ips-node/etc/crl-dir/B-TrustTestOperationalACA.crl
+#
+
+
+${download1}
+
+${download2}
+
+openssl crl -in $home/new/B-TrustTestOperationalQCA.crl -inform DER -text -noout  > $home/new/report1
+openssl crl -in $home/new/B-TrustTestOperationalACA.crl -inform DER -text -noout  > $home/new/report2
+
+md5sum $new_crl1 >> $home/new/report1
+md5sum $new_crl2 >> $home/new/report2
+md5sum $old_crl1 >> $home/new/report1
+md5sum $old_crl2 >> $home/new/report2
+
+if [ -s $home/new/report1 ]
+ then
+  if ! diff ${new_crl1}  ${old_crl1}
+     then
+        mv ${new_crl1}  ${old_crl1}
+  fi
+fi
+if [ -s $home/new/report2 ]
+ then
+  if ! diff ${new_crl2}  ${old_crl2}
+   then
+    mv ${new_crl2}  ${old_crl2}
+  fi
+fi
