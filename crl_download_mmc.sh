489050f826bcc21a620a8b73f02f53b0cbf05ca7
Georgi <30492692+georgi1234@users.noreply.github.com>
2021-03-19 12:07:51 +0200

    Create crl_download_mmc.sh

---
+++ crl_download_mmc.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+##Created by Georgi Dimitrov
+##CRL download and check
+
+#vars
+export home=/home/app/MontranConfig/etc/cli-dir/
+download1=$(wget http://crl.b-trust.org/repository/B-TrustOperationalQCA.crl -O $home/new/B-TrustOperationalQCA.crl)
+download2=$(wget http://crl.b-trust.org/repository/B-TrustOperationalACA.crl -O $home/new/B-TrustOperationalACA.crl)
+download3=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalQCA.crl -O $home/new/B-TrustTestOperationalQCA.crl)
+download4=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalACA.crl -O $home/new/B-TrustTestOperationalACA.crl)
+new_crl1=$home/new/B-TrustOperationalQCA.crl
+new_crl2=$home/new/B-TrustOperationalACA.crl
+old_crl1=$home/B-TrustOperationalQCA.crl
+old_crl2=$home/B-TrustOperationalACA.crl
+new_crl3=$home/new/B-TrustTestOperationalQCA.crl
+new_crl4=$home/new/B-TrustTestOperationalACA.crl
+old_crl3=$home/B-TrustTestOperationalQCA.crl
+old_crl4=$home/B-TrustTestOperationalACA.crl
+
+
+
+#
+
+
+${download1}
+
+${download2}
+
+${download3}
+
+${download4}
+
+openssl crl -in $home/new/B-TrustOperationalQCA.crl -inform DER -text -noout  > $home/new/report1
+openssl crl -in $home/new/B-TrustOperationalACA.crl -inform DER -text -noout  > $home/new/report2
+openssl crl -in $home/new/B-TrustTestOperationalQCA.crl -inform DER -text -noout  > $home/new/report3
+openssl crl -in $home/new/B-TrustTestOperationalACA.crl -inform DER -text -noout  > $home/new/report4
+
+
+
+
+md5sum $new_crl1 >> $home/new/report1
+md5sum $new_crl2 >> $home/new/report2
+md5sum $old_crl1 >> $home/new/report1
+md5sum $old_crl2 >> $home/new/report2
+md5sum $new_crl3 >> $home/new/report3
+md5sum $new_crl4 >> $home/new/report4
+md5sum $old_crl3 >> $home/new/report3
+md5sum $old_crl4 >> $home/new/report4
+
+
+if [ -s $home/new/report1 ]
+ then
+  if ! diff ${new_crl1}  ${old_crl1}
+     then
+        mv ${new_crl1}  ${old_crl1}
+  fi
+fi
+if [ -s $home/new/report2 ]
+ then
+  if ! diff ${new_crl2}  ${old_crl2}
+   then
+    mv ${new_crl2}  ${old_crl2}
+  fi
+fi
+if [ -s $home/new/report3 ]
+ then
+  if ! diff ${new_crl3}  ${old_crl3}
+     then
+        mv ${new_crl3}  ${old_crl3}
+  fi
+fi
+if [ -s $home/new/report4 ]
+ then
+  if ! diff ${new_crl4}  ${old_crl4}
+   then
+    mv ${new_crl4}  ${old_crl4}
+  fi
+fi
