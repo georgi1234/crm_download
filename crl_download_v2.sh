80af97c10ee8b48fa24a955ae6eeba28d6a471da
Georgi <30492692+georgi1234@users.noreply.github.com>
2021-03-19 11:17:51 +0200

    Update crl_download_v2.sh
    Last merged working version

--- crl_download_v2.sh
+++ crl_download_v2.sh
@@ -3,11 +3,11 @@
 ##CRL download and check
 
 #vars
-export home=/app/ips-node/etc/crl-dir/
-download1=$(wget http://crl.b-trust.org/repository/B-TrustOperationalQCA.crl -O $home/new/B-TrustOperationalQCA.crl)
-download2=$(wget http://crl.b-trust.org/repository/B-TrustOperationalACA.crl -O $home/new/B-TrustOperationalACA.crl)
-download3=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalQCA.crl -O $home/new/B-TrustTestOperationalQCA.crl)
-download4=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalACA.crl -O $home/new/B-TrustTestOperationalACA.crl)
+export home=/app/ips-node/etc/
+download1=$(wget http://crl.b-trust.org/repository/B-TrustOperationalQCA.crl -O $home/crl-dir/new/B-TrustOperationalQCA.crl)
+download2=$(wget http://crl.b-trust.org/repository/B-TrustOperationalACA.crl -O $home/crl-dir/new/B-TrustOperationalACA.crl)
+download3=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalQCA.crl -O $home/crl-dir/new/B-TrustTestOperationalQCA.crl)
+download4=$(wget http://crltest.b-trust.org/repository/B-TrustTestOperationalACA.crl -O $home/crl-dir/new/B-TrustTestOperationalACA.crl)
 new_crl1=/app/ips-node/etc/crl-dir/new/B-TrustOperationalQCA.crl
 new_crl2=/app/ips-node/etc/crl-dir/new/B-TrustOperationalACA.crl
 new_crl3=/app/ips-node/etc/crl-dir/new/B-TrustTestOperationalQCA.crl
@@ -19,6 +19,7 @@ old_crl4=/app/ips-node/etc/crl-dir/B-TrustTestOperationalACA.crl
 #
 
 
+
 ${download1}
 
 ${download2}
@@ -27,44 +28,44 @@ ${download3}
 
 ${download4}
 
-openssl crl -in $home/new/B-TrustOperationalQCA.crl -inform DER -text -noout  > $home/new/report1
-openssl crl -in $home/new/B-TrustOperationalACA.crl -inform DER -text -noout  > $home/new/report2
-openssl crl -in $home/new/B-TrustTestOperationalQCA.crl -inform DER -text -noout  > $home/new/report3
-openssl crl -in $home/new/B-TrustTestOperationalACA.crl -inform DER -text -noout  > $home/new/report4
-md5sum $new_crl1 >> $home/new/report1
-md5sum $new_crl2 >> $home/new/report2
-md5sum $old_crl1 >> $home/new/report1
-md5sum $old_crl2 >> $home/new/report2
+openssl crl -in $home/crl-dir/new/B-TrustOperationalQCA.crl -inform DER -text -noout  > $home/crl-dir/new/report1
+openssl crl -in $home/crl-dir/new/B-TrustOperationalACA.crl -inform DER -text -noout  > $home/crl-dir/new/report2
+openssl crl -in $home/crl-dir/new/B-TrustTestOperationalQCA.crl -inform DER -text -noout  > $home/crl-dir/new/report3
+openssl crl -in $home/crl-dir/new/B-TrustTestOperationalACA.crl -inform DER -text -noout  > $home/crl-dir/new/report4
+md5sum $new_crl1 >> $home/crl-dir/new/report1
+md5sum $new_crl2 >> $home/crl-dir/new/report2
+md5sum $old_crl1 >> $home/crl-dir/new/report1
+md5sum $old_crl2 >> $home/crl-dir/new/report2
 
-md5sum $new_crl3 >> $home/new/report3
-md5sum $new_crl4 >> $home/new/report4
-md5sum $old_crl3 >> $home/new/report3
-md5sum $old_crl3 >> $home/new/report4
+md5sum $new_crl3 >> $home/crl-dir/new/report3
+md5sum $new_crl4 >> $home/crl-dir/new/report4
+md5sum $old_crl3 >> $home/crl-dir/new/report3
+md5sum $old_crl4 >> $home/crl-dir/new/report4
 
 
 
-if [ -s $home/new/report1 ]
+if [ -s $home/crl-dir/new/report1 ]
  then
   if ! diff ${new_crl1}  ${old_crl1}
      then
         mv ${new_crl1}  ${old_crl1}
   fi
 fi
-if [ -s $home/new/report2 ]
+if [ -s $home/crl-dir/new/report2 ]
  then
   if ! diff ${new_crl2}  ${old_crl2}
    then
     mv ${new_crl2}  ${old_crl2}
   fi
 fi
-if [ -s $home/new/report3 ]
+if [ -s $home/crl-dir/new/report3 ]
  then
   if ! diff ${new_crl3}  ${old_crl3}
    then
     mv ${new_crl3}  ${old_crl3}
   fi
 fi
-if [ -s $home/new/report4 ]
+if [ -s $home/crl-dir/new/report4 ]
  then
   if ! diff ${new_crl4}  ${old_crl4}
    then
